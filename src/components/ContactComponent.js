3be664bb8e50bb7d01ec0149b58f3deca8cb3a25
vishalyelme <vishalyelme8698@gmail.com>
2020-06-06 20:24:56 +0530

    Control Forms

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -106,6 +106,22 @@ class Contact extends Component{
                                     </Input>
                                 </Col>
                             </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="message" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Input type="textarea" id="message" name="message"
+                                        rows="12"
+                                        value={this.state.message}
+                                        onChange={this.handleInputChange}></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                        Send Feedback
+                                    </Button>
+                                </Col>
+                            </FormGroup>
                         </Form>
                     </div>
                 </div>
